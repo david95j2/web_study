73b2471cac27cb36d81c996a9e8a7b1f58d19f40
Hanwook Kim <gksdnr89@naver.com>
2017-03-01 23:40:08 +0900

    throws Exception에서 SQLException으로 변경

--- HelloProject/src/com/dasol/jdbc/ConnectionProvider.java
+++ HelloProject/src/com/dasol/jdbc/ConnectionProvider.java
@@ -2,9 +2,10 @@ package com.dasol.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConnectionProvider {
-	public static Connection getConnection() throws Exception {
+	public static Connection getConnection() throws SQLException {
 		return DriverManager.getConnection("jdbc:apache:commons:dbcp:myboard");
 	}
 }
