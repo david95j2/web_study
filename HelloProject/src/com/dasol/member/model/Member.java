09acef5ba411f5c74298c51f0c6605459b5ee76f
Hanwook Kim <gksdnr89@naver.com>
2017-03-02 23:10:19 +0900

    데이터 베이스 테이블 변경으로 인한 Member 클래스 수정

--- HelloProject/src/com/dasol/member/model/Member.java
+++ HelloProject/src/com/dasol/member/model/Member.java
@@ -3,16 +3,40 @@ package com.dasol.member.model;
 import java.util.Date;
 
 public class Member {
+	private int memberId;
 	private String email;
 	private String password;
+	private String nickname;
 	private Date regdate;
-	private String profile_image;
-
-	public Member(String email, String password, Date regdate, String profile_image) {
+	private String profileImage;
+	private String registerCode;
+	private boolean registerCheck;
+	
+	public Member(String email, String password, String nickname, Date regdate, 
+			String profileImage, String registerCode, boolean registerCheck) {
 		this.email = email;
 		this.password = password;
+		this.nickname = nickname;
 		this.regdate = regdate;
-		this.profile_image = profile_image;
+		this.profileImage = profileImage;
+		this.registerCode = registerCode;
+		this.registerCheck = registerCheck;
+	}
+	
+	public Member(int memberId, String email, String password, String nickname, Date regdate, 
+			String profileImage, String registerCode, boolean registerCheck) {
+		this.memberId = memberId;
+		this.email = email;
+		this.password = password;
+		this.nickname = nickname;
+		this.regdate = regdate;
+		this.profileImage = profileImage;
+		this.registerCode = registerCode;
+		this.registerCheck = registerCheck;
+	}
+
+	public int getMemberId() {
+		return memberId;
 	}
 
 	public String getEmail() {
@@ -23,12 +47,24 @@ public class Member {
 		return password;
 	}
 
+	public String getNickname() {
+		return nickname;
+	}
+
 	public Date getRegdate() {
 		return regdate;
 	}
 
-	public String getProfile_image() {
-		return profile_image;
+	public String getProfileImage() {
+		return profileImage;
+	}
+
+	public String getRegisterCode() {
+		return registerCode;
+	}
+
+	public boolean isRegisterCheck() {
+		return registerCheck;
 	}
 
 }
