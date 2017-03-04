31211940f578f6f55b20efd8e5073e249fa9a064
Hanwook Kim <gksdnr89@naver.com>
2017-03-04 23:10:07 +0900

    정규표현식 삭제 (클라이언트에서 함)

--- HelloProject/src/com/dasol/member/service/JoinRequest.java
+++ HelloProject/src/com/dasol/member/service/JoinRequest.java
@@ -1,8 +1,6 @@
 package com.dasol.member.service;
 
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class JoinRequest {
 	private String email;
@@ -31,55 +29,6 @@ public class JoinRequest {
 	
 	public void setConfirmPassword(String confirmPassword) {
 		this.confirmPassword = confirmPassword;
-	}
-	
-	public void validate(Map<String, Boolean> errors) {
-		checkEmpty(errors, email, "email");
-		checkEmpty(errors, password, "password");
-		checkEmpty(errors, confirmPassword, "confirmPassword");
-		subValidate(errors);
-	}
-	
-	private void subValidate(Map<String, Boolean> errors) {
-		if(!errors.containsKey("email")) {
-			checkEmailRegEx(errors, email, "emailRegEx");
-		}
-		
-		if(!errors.containsKey("password")) {
-			checkPasswordRegEx(errors, password, "passwordRegEx");
-		}
-		
-		if(!errors.containsKey("confirmPassword")) {
-			if(!comparePassword()) {
-				errors.put("noMatch", Boolean.TRUE);
-			}
-		}
-	}
-	
-	public boolean comparePassword() {
-		return password != null && password.equals(confirmPassword);
-	}
-	
-	private void checkEmpty(Map<String, Boolean> errors, String value, String fieldName) {
-		if(value == null || value.isEmpty()) {
-			errors.put(fieldName, Boolean.TRUE);
-		}
-	}
-	
-	private void checkEmailRegEx(Map<String, Boolean> errors, String value, String errorName) {
-		Pattern pattern = Pattern.compile("^[_a-zA-Z0-9-\\.]+@[\\.a-zA-Z0-9-]+\\.[a-zA-Z]+$");
-		Matcher matcher = pattern.matcher(value);
-		if(!matcher.matches()) {
-			errors.put(errorName, Boolean.TRUE);
-		}
-	}
-	
-	private void checkPasswordRegEx(Map<String, Boolean> errors, String value, String errorName) {
-		Pattern pattern = Pattern.compile("^(?=.*[a-zA-Z]+)(?=.*[!@#$%^*+=-]|.*[0-9]+).{8,16}$");
-		Matcher matcher = pattern.matcher(value);
-		if(!matcher.matches()) {
-			errors.put(errorName, Boolean.TRUE);
-		}
 	}
 	
 }
