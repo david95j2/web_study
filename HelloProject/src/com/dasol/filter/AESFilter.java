91201410f808882b9cc02f0e5ffb5520192a9d14
Hanwook Kim <gksdnr89@naver.com>
2017-03-04 23:07:19 +0900

    데이터 암호화 필터 추가

---
+++ HelloProject/src/com/dasol/filter/AESFilter.java
@@ -0,0 +1,32 @@
+package com.dasol.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+public class AESFilter implements Filter {
+
+	@Override
+	public void destroy() {
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+			throws IOException, ServletException {
+		System.out.println("filter");
+		AESRequestWrapper wrapper = new AESRequestWrapper((HttpServletRequest)req);
+		chain.doFilter(wrapper, resp);
+	}
+
+	@Override
+	public void init(FilterConfig config) throws ServletException {
+		
+	}
+
+}
