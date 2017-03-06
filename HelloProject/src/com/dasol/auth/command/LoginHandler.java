2fdb2224a6e605e5303828757a941453170d8720
Hanwook Kim <gksdnr89@naver.com>
2017-03-06 15:20:13 +0900

    LoginHandler 클래스 추가
    - 로그인 페이지 컨트롤러

---
+++ HelloProject/src/com/dasol/auth/command/LoginHandler.java
@@ -0,0 +1,57 @@
+package com.dasol.auth.command;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.dasol.auth.service.LoginFailException;
+import com.dasol.auth.service.LoginService;
+import com.dasol.auth.service.User;
+import com.dasol.mvc.command.CommandHandler;
+
+public class LoginHandler implements CommandHandler {
+	private static final String FORM_VIEW = "/WEB-INF/view/loginForm.jsp";
+	private LoginService loginService = new LoginService();
+
+	@Override
+	public String process(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		if (request.getMethod().equalsIgnoreCase("GET")) {
+			return processForm(request, response);
+		} else if (request.getMethod().equalsIgnoreCase("POST")) {
+			return processSubmit(request, response);
+		} else {
+			response.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+			return null;
+		}
+	}
+
+	private String processForm(HttpServletRequest request, HttpServletResponse response) {
+		return FORM_VIEW;
+	}
+	
+	private String processSubmit(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String email = trim(request.getParameter("email"));
+		String password = trim(request.getParameter("password"));
+		
+		Map<String, Boolean> errors = new HashMap<>();
+		request.setAttribute("errors", errors);
+		
+		try {
+			User user = loginService.login(email, password);
+			request.getSession().setAttribute("authUser", user);
+			response.sendRedirect(request.getContextPath() + "/index.jsp");
+			return null;
+		} catch (LoginFailException e) {
+			errors.put("idOrPwNotMatch", Boolean.TRUE);
+			return FORM_VIEW;
+		}
+		
+	}
+	
+	private String trim(String str) {
+		return str == null ? null : str.trim();
+	}
+
+}
