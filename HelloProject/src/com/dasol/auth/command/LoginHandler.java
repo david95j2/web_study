e4b736d97dc4017754931a62c2b6662c98f33bd6
Hanwook Kim <gksdnr89@naver.com>
2017-03-09 22:19:43 +0900

    로그인 유지 관련 디테일 수정
    - 쿠키 발급
    - 리멤버 체크 확인

--- HelloProject/src/com/dasol/auth/command/LoginHandler.java
+++ HelloProject/src/com/dasol/auth/command/LoginHandler.java
@@ -3,6 +3,7 @@ package com.dasol.auth.command;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -34,15 +35,24 @@ public class LoginHandler implements CommandHandler {
 	private String processSubmit(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		String email = trim(request.getParameter("email"));
 		String password = trim(request.getParameter("password"));
+		String remember = request.getParameter("remember");
 		
 		Map<String, Boolean> errors = new HashMap<>();
 		request.setAttribute("errors", errors);
 		
 		try {
-			User user = loginService.login(email, password);
+			User user = loginService.login(email, password, remember);
+			
+			if (user.getRememberToken() != null) {
+				Cookie cookie = new Cookie("aT", user.getRememberToken());
+				cookie.setMaxAge(14*24*60*60);
+				response.addCookie(cookie);
+			}
+			
 			request.getSession().setAttribute("authUser", user);
 			response.sendRedirect(request.getContextPath() + "/index.jsp");
 			return null;
+			
 		} catch (LoginFailException e) {
 			errors.put("idOrPwNotMatch", Boolean.TRUE);
 			return FORM_VIEW;
--- HelloProject/src/com/dasol/auth/service/LoginService.java
+++ HelloProject/src/com/dasol/auth/service/LoginService.java
@@ -4,14 +4,21 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 import com.dasol.jdbc.ConnectionProvider;
+import com.dasol.jdbc.JdbcUtil;
 import com.dasol.member.dao.MemberDAO;
 import com.dasol.member.model.Member;
+import com.dasol.util.TokenGenerator;
 
 public class LoginService {
 	MemberDAO memberDAO = new MemberDAO();
 
-	public User login(String email, String password) {
-		try (Connection conn = ConnectionProvider.getConnection()){
+	public User login(String email, String password, String remember) {
+		Connection conn = null;
+		String token = null;
+		try {
+			conn = ConnectionProvider.getConnection();
+			conn.setAutoCommit(false);
+			
 			Member member = memberDAO.selectByEmail(conn, email);
 
 			if (member == null) {
@@ -21,9 +28,17 @@ public class LoginService {
 			if (!member.matchPassword(password)) {
 				throw new LoginFailException();
 			}
+
+			if (remember != null) { // 체크가 없었던 유저가 로그인 시
+				token = TokenGenerator.getTokenKey(); // 토큰키를 생성해서 dao에 넣어준다.
+				memberDAO.updateToken(conn, member.getMemberId(), token);
+			}
 			
-			return new User(member.getEmail(), member.isRegisterCheck());
+			conn.commit();
+			return new User(member.getEmail(), member.isRegisterCheck(), token);
+			
 		} catch (SQLException e) {
+			JdbcUtil.rollback(conn);
 			throw new RuntimeException(e);
 		}
 
