6330706e5bf443b1da7f92a0b782f38cd3fe5bd9
Hanwook Kim <gksdnr89@naver.com>
2017-03-14 14:44:30 +0900

    User 수정
    - 패스워드 설정 여부 체크 위해 hasPassword 필드 및 메소드 추가

--- HelloProject/src/com/dasol/auth/service/User.java
+++ HelloProject/src/com/dasol/auth/service/User.java
@@ -1,16 +1,25 @@
 package com.dasol.auth.service;
 
 public class User {
+	private int memberId;
 	private String email;
 	private boolean registerCheck;
 	private String rememberToken;
+	private boolean hasPassword;
 
-	public User(String email, boolean registerCheck, String rememberToken) {
+	public User(int memberId, String email, boolean registerCheck, String rememberToken,
+			boolean hasPassword) {
+		this.memberId = memberId;
 		this.email = email;
 		this.registerCheck = registerCheck;
 		this.rememberToken = rememberToken;
+		this.hasPassword = hasPassword;
 	}
 
+	public int getMemberId() {
+		return memberId;
+	}
+	
 	public String getEmail() {
 		return email;
 	}
@@ -23,4 +32,11 @@ public class User {
 		return rememberToken;
 	}
 	
+	public boolean getHasPassword() {
+		return hasPassword;
+	}
+	
+	public void setHasPassword(boolean check){
+		this.hasPassword = check;
+	}
 }
