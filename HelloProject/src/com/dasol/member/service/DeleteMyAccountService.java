f4c0bab8e5d231ec2fce521d2c46ef353a03b963
Hanwook Kim <gksdnr89@naver.com>
2017-03-14 14:43:00 +0900

    DeleteMyAccountService 추가
    - 계정 삭제 서비스

---
+++ HelloProject/src/com/dasol/member/service/DeleteMyAccountService.java
@@ -0,0 +1,40 @@
+package com.dasol.member.service;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.dasol.jdbc.ConnectionProvider;
+import com.dasol.jdbc.JdbcUtil;
+import com.dasol.member.dao.MemberDAO;
+import com.dasol.member.model.Member;
+
+public class DeleteMyAccountService {
+	MemberDAO memberDAO = new MemberDAO();
+
+	public void deleteAccount(int memberId, String password) {
+		Connection conn = null;
+
+		try {
+			conn = ConnectionProvider.getConnection();
+			conn.setAutoCommit(false);
+			Member member = memberDAO.selectByMemberId(conn, memberId);
+
+			if (member == null) {
+				throw new MemberNotFoundException();
+			}
+
+			if (!member.matchPassword(password)) {
+				throw new InvalidPasswordException();
+			}
+
+			memberDAO.deleteMyAccount(conn, memberId);
+			conn.commit();
+
+		} catch (SQLException e) {
+			JdbcUtil.rollback(conn);
+			throw new RuntimeException(e);
+		} finally {
+			JdbcUtil.close(conn);
+		}
+	}
+}
