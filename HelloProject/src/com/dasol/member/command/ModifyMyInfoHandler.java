9b08d2630b632f7735f76da4a7293c15645e14fd
Hanwook Kim <gksdnr89@naver.com>
2017-03-14 14:38:03 +0900

    ModifyMyInfoHandler 추가
    - GET : 정보 읽기
    - POST : 수정 정보 요청

---
+++ HelloProject/src/com/dasol/member/command/ModifyMyInfoHandler.java
@@ -0,0 +1,88 @@
+package com.dasol.member.command;
+
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.dasol.auth.service.User;
+import com.dasol.member.service.ModifyMyInfoService;
+import com.dasol.member.service.MyInfo;
+import com.dasol.member.service.ReadMyInfoService;
+import com.dasol.mvc.command.CommandHandler;
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+import com.sun.glass.ui.Application;
+
+public class ModifyMyInfoHandler implements CommandHandler {
+	private static final String FORM_VIEW = "/WEB-INF/view/modifyMyInfoForm.jsp";
+	private ReadMyInfoService readMyInfoService = new ReadMyInfoService();
+	private ModifyMyInfoService modifyMyInfoService = new ModifyMyInfoService();
+
+	@Override
+	public String process(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		if (request.getMethod().equalsIgnoreCase("GET")) {
+			return processForm(request, response);
+		} else if (request.getMethod().equalsIgnoreCase("POST")) {
+			return processSubmit(request, response);
+		} else {
+			response.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+			return null;
+		}
+	}
+
+	private String processForm(HttpServletRequest request, HttpServletResponse response) {
+		User authUser = (User) request.getSession().getAttribute("authUser");
+		int memberId = authUser.getMemberId();
+		MyInfo myInfo = readMyInfoService.getMyInfo(memberId);
+		
+		if (myInfo.getProfileImage() == null)
+			myInfo.setDefaultProfile();
+		
+		request.setAttribute("myinfo", myInfo);
+		return FORM_VIEW;
+	}
+
+	private String processSubmit(HttpServletRequest request, HttpServletResponse response) {
+		String path = request.getSession().getServletContext().getRealPath("profileimg");
+		User authUser = (User) request.getSession().getAttribute("authUser");
+		int memberId = authUser.getMemberId();
+
+		int size = 1024 * 1024 * 10;
+		String file = "";
+		String oriFile = "";
+
+		try {
+			MultipartRequest multi 
+				= new MultipartRequest(request, path, size, "UTF-8", new DefaultFileRenamePolicy());
+
+			String nickname = multi.getParameter("nickname");
+			Enumeration files = multi.getFileNames();
+
+			String str = (String) files.nextElement();
+			
+			file = multi.getFilesystemName(str);
+			oriFile = multi.getOriginalFileName(str);
+			
+			String profileImage = file;
+			
+			if(file != null)
+				profileImage = "profileimg\\" + file;
+			
+			MyInfo myInfo = new MyInfo(nickname, profileImage);
+			myInfo = modifyMyInfoService.modify(myInfo, memberId);
+			
+			if (myInfo.getProfileImage() == null)
+				myInfo.setDefaultProfile();
+			
+			request.setAttribute("myinfo", myInfo);
+			return FORM_VIEW;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+
+	}
+
+}
