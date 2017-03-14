53a39d0e2a92773aab02008901b61db8f1c10f3a
Hanwook Kim <gksdnr89@naver.com>
2017-03-14 14:36:05 +0900

    ReadMyInfoService 추가
    - 회원 정보 읽기(프로필이미지, 닉네임)

---
+++ HelloProject/src/com/dasol/member/service/ReadMyInfoService.java
@@ -0,0 +1,29 @@
+package com.dasol.member.service;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.dasol.jdbc.ConnectionProvider;
+import com.dasol.member.dao.MemberDAO;
+import com.dasol.member.model.Member;
+
+public class ReadMyInfoService {
+	MemberDAO memberDAO = new MemberDAO();
+
+	public MyInfo getMyInfo(int memberId) {
+
+		try (Connection conn = ConnectionProvider.getConnection()) {
+			Member member = memberDAO.selectByMemberId(conn, memberId);
+
+			if (member == null) {
+				throw new RuntimeException();
+			}
+
+			return new MyInfo(member.getNickname(), member.getProfileImage());
+
+		} catch (SQLException e) {
+			throw new RuntimeException();
+		}
+		
+	}
+}
