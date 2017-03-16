f6ca427b0cbfe5d008badf49bd9988e3d55bc8bb
Hanwook Kim <gksdnr89@naver.com>
2017-03-16 21:33:36 +0900

    FindPasswordService 클래스 추가
    - 비밀번호 찾기 서비스 로직

---
+++ HelloProject/src/com/dasol/member/service/FindPasswordService.java
@@ -0,0 +1,34 @@
+package com.dasol.member.service;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.dasol.jdbc.ConnectionProvider;
+import com.dasol.member.dao.MemberDAO;
+import com.dasol.member.model.Member;
+import com.dasol.util.SendEmail;
+
+public class FindPasswordService {
+	MemberDAO memberDAO = new MemberDAO();
+	
+	public void find(String email) {
+		
+		try (Connection conn = ConnectionProvider.getConnection()) {
+			
+			Member member = memberDAO.selectByEmail(conn, email);
+			
+			if (member == null) {
+				throw new MemberNotFoundException();
+			}
+			
+			MailInfo mailInfo = new MailInfo();
+			mailInfo.setEmail(member.getEmail());
+			mailInfo.setPasswordContent();
+			SendEmail.send(mailInfo);
+			
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		
+	}
+}
