7e4db7b7a00b1bb6623fa514988cd8d86419f4ad
Hanwook Kim <gksdnr89@naver.com>
2017-03-16 15:01:22 +0900

    ChangePasswordService에 setPwd() 메소드 추가
    - 비밀번호 설정 관련 서비스 로직 추가

--- HelloProject/src/com/dasol/member/service/ChangePasswordService.java
+++ HelloProject/src/com/dasol/member/service/ChangePasswordService.java
@@ -20,6 +20,7 @@ public class ChangePasswordService {
 			Member member = memberDAO.selectByMemberId(conn, memberId);
 
 			if (member == null) {
+				JdbcUtil.rollback(conn);
 				throw new MemberNotFoundException();
 			}
 			
@@ -29,7 +30,33 @@ public class ChangePasswordService {
 				}
 			}
 			
-			memberDAO.changePassword(conn, newPwd, memberId);
+			member.changePwd(newPwd);
+			memberDAO.updateMyInfo(conn, member);
+			conn.commit();
+
+		} catch (SQLException e) {
+			JdbcUtil.rollback(conn);
+			throw new RuntimeException(e);
+		} finally {
+			JdbcUtil.close(conn);
+		}
+	}
+	
+	public void setPwd(String email, String newPwd) {
+		Connection conn = null;
+		try {
+			conn = ConnectionProvider.getConnection();
+			conn.setAutoCommit(false);
+			
+			Member member = memberDAO.selectByEmail(conn, email);
+
+			if (member == null) {
+				JdbcUtil.rollback(conn);
+				throw new MemberNotFoundException();
+			}
+			
+			member.changePwd(newPwd);
+			memberDAO.updateMyInfo(conn, member);
 			conn.commit();
 
 		} catch (SQLException e) {
