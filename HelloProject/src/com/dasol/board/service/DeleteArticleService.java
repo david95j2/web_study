10edd04b8cf005f2a13f987452fe9a59d3b81783
Hanwook Kim <gksdnr89@naver.com>
2017-03-20 20:26:03 +0900

    게시글 삭제 기능 추가

---
+++ HelloProject/src/com/dasol/board/service/DeleteArticleService.java
@@ -0,0 +1,53 @@
+package com.dasol.board.service;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.dasol.board.dao.ArticleContentDAO;
+import com.dasol.board.dao.ArticleDAO;
+import com.dasol.board.model.Article;
+import com.dasol.board.model.ArticleContent;
+import com.dasol.jdbc.ConnectionProvider;
+import com.dasol.jdbc.JdbcUtil;
+
+public class DeleteArticleService {
+	
+	ArticleContentDAO articleContentDAO = new ArticleContentDAO();
+	ArticleDAO articleDAO = new ArticleDAO();
+	
+	public void delete(int no, int memberId) {
+		Connection conn = null;
+		try {
+			conn = ConnectionProvider.getConnection();
+			conn.setAutoCommit(false);
+			
+			Article article = articleDAO.selectById(conn, no);
+			if(article == null) {
+				throw new ArticleNotFoundException();
+			}
+			
+			ArticleContent content = articleContentDAO.selectById(conn, no);
+			if(content == null) {
+				throw new ArticleContentNotFoundException();
+			}
+			
+			if(!PermissionChecker.canDo(memberId, article)) {
+				throw new PermissionDeniedException();
+			}
+			
+			articleDAO.delete(conn, no);
+			articleContentDAO.delete(conn, no);
+			conn.commit();
+			
+		} catch (SQLException e) {
+			JdbcUtil.rollback(conn);
+			throw new RuntimeException(e);
+		} catch (PermissionDeniedException e) {
+			JdbcUtil.rollback(conn);
+			throw e;
+		} finally {
+			JdbcUtil.close(conn);
+		}
+		
+	}
+}
